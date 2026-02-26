2612f43916b87e8bd819429ff62076181e34ec35
Dadimka <vadim.khalilov@inbox.ru>
2026-02-27 02:27:45 +0500

    Base referencs

--- Example05_Base/Program.cs
+++ Example05_Base/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int c = new Random().Next(1, 10);
+int d = new Random().Next(1, 10);
+int e = new Random().Next(1, 10);
+int max = a;
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+if (d > max)
+    max = d;
+if (e > max)
+    max = e;
+Console.WriteLine($"The maximum value is: {max}, generated from the numbers: {a}, {b}, {c}, {d}, {e}.");        
