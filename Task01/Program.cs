864e8e9b901efaa2fe7b1a051b1902e362afc949
Dadimka <vadim.khalilov@inbox.ru>
2026-03-05 18:49:25 +0500

    First task

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -5,7 +5,15 @@
 //4 -> 16
 //-3 -> 9
 //-7 -> 49
+//Console.WriteLine("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int square = number * number;
+//Console.WriteLine($"Квадрат числа {number} равен {square}");
+// Написать программу, которая спрашивает у пользователя число и степень,
+// в которую нужно возвести это число, и выводит результат на экран.
 Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int square = number * number;
-Console.WriteLine($"Квадрат числа {number} равен {square}");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+long result = (long)Math.Pow(num, degree);
+Console.WriteLine($"Результат: {result}");
