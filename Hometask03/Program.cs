46e77a4fa845dd7ba892c8ac42921e57be52c492
Dadimka <vadim.khalilov@inbox.ru>
2026-03-05 19:27:12 +0500

    Fourth Hometask

---
+++ Hometask03/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Чётные числа от 1 до {N}:");
+for (int i = 2; i <= N; i += 2)
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
