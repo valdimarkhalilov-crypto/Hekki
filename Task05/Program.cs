55bfc43408b66b61e46d927fcaed66a296299b1a
Dadimka <vadim.khalilov@inbox.ru>
2026-03-05 19:19:35 +0500

    Fifth task

---
+++ Task05/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number % 10;
+Console.WriteLine($"Последняя цифра числа {number} равна {lastDigit}");
