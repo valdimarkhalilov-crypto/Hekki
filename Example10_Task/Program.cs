fdd72ce5d9302b7ebbcb79fe72054518e47b8195
Dadimka <vadim.khalilov@inbox.ru>
2026-03-05 14:58:34 +0500

    Text exterminated

---
+++ Example10_Task/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = "-Я думаю, - сказал князь, улыбаясь, - что," 
++ " ежели бы вас послали вместо нашего милого Винценгероде, "
++ " вы бы взяли приступом согласие прусского короля."
++ " Вы так красноречивы. Вы дадите мне чаю?";
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+newText = Replace(newText, 'к', 'К');
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
