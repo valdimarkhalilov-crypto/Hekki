2d3a60f2ff1a4c0d3d4fcc8ccc7cebb6d6bff599
Dadimka <vadim.khalilov@inbox.ru>
2026-03-05 19:36:25 +0500

    Second lesson third task

---
+++ Task03_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 % num2 == 0)
+{
+    Console.WriteLine($"{num2} кратно {num1}");
+}
+else
+{
+    Console.WriteLine($"{num2} не кратно {num1}");
+}
