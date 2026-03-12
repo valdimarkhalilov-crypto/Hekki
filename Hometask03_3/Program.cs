087ecf1af544e1e7bd52a0fecdd8892c98a0fe4b
Dadimka <vadim.khalilov@inbox.ru>
2026-03-12 16:40:48 +0500

    Third task lesson 3

---
+++ Hometask03_3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте массив из 10 целых чисел. Найдите среднее значение элементов
+//массива.
+using System;
+namespace Hometask03_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rand = new Random();
+
+            Console.WriteLine("Массив из 10 целых чисел:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 11);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            double average = array.Average();
+            Console.WriteLine($"Среднее значение элементов массива: {average}");
+        }
+    }
+}
