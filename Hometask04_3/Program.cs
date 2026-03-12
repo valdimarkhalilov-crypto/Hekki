6fd3cb9db4274926e463ad778e3c91accecd8a88
Dadimka <vadim.khalilov@inbox.ru>
2026-03-12 16:47:15 +0500

    Fourth task lessson 3

---
+++ Hometask04_3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте массив целых чисел. Напишите программу, которая удаляет все
+//отрицательные числа из массива и возвращает новый массив, содержащий только
+//неотрицательные числа.
+using System;
+using System.Linq;
+namespace Hometask04_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите массив целых чисел через пробел:");
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int[] nonNegativeArray = array.Where(num => num >= 0).ToArray();
+
+            Console.WriteLine("Массив без отрицательных чисел:");
+            foreach (int num in nonNegativeArray)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
